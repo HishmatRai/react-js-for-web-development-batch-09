a053c30f2ac7a4f28165b93b4b47e61346dca84b
hishmatrai <hishmatrai.2018@gamil.com>
2024-02-26 05:45:40 -0800

    update

--- src/pages/blog-details.js
+++ src/pages/blog-details.js
@@ -1,32 +1,40 @@
-import React ,{useEffect,useState}from "react";
-import {useLocation,useNavigate} from 'react-router-dom';
-import { doc, onSnapshot,getFirestore } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { doc, onSnapshot, getFirestore } from "firebase/firestore";
 import firebase from "../config/firebase";
+import {
+    FacebookShareButton,
+    FacebookIcon,
+} from "react-share";
 const db = getFirestore(firebase);
-const BlogDetails = ()=>{
+const BlogDetails = () => {
     const location = useLocation();
     const navigate = useNavigate()
-    const [blog,setBlog] = useState()
-    console.log("location>>>>>>>>",location.pathname.slice(14))
+    const [blog, setBlog] = useState()
+    console.log("location>>>>>>>>", location.pathname.slice(14))
     let path = location.pathname.slice(14)
-    useEffect(()=>{
+    useEffect(() => {
         const unsub = onSnapshot(doc(db, "blogs", path), (doc) => {
             // console.log("Current data: ", doc.data());
-            if(doc.data() === undefined){
+            if (doc.data() === undefined) {
                 navigate("/about-us")
-            }else{
+            } else {
                 setBlog(doc.data())
 
             }
         });
-        
-    },[])
-    return(
+
+    }, [])
+    return (
         <div>
             <h1>Blog Details</h1>
             <h2>{blog?.title}</h2>
             <p>{blog?.details}</p>
-            <img src={blog?.fileUrl} width={"100%"}/>
+            <img src={blog?.fileUrl} width={"100%"} />
+            <FacebookShareButton url={`https://ihunar-blog.netlify.app/blog-details/${path}`}>
+                <FacebookIcon size={50} round={true} />
+                <p>Path :- {`https://ihunar-blog.netlify.app/blog-details/${path}`}</p>
+            </FacebookShareButton>
         </div>
     )
 }
